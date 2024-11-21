26606835011676bcc428d14ec74861c749cf40d7
Olga Akukin <olyapa@gmail.com>
2024-11-21 02:42:29 +0100

    grebannye kavychki

--- brain_games/rules.py
+++ brain_games/rules.py
@@ -1,7 +1,7 @@
 BRAIN_CALC_RULES = "What is the result of the expression?"
 BRAIN_PROGRESSION_RULES = "What number is missing in the progression?"
 BRAIN_GCD_RULES = "Find the greatest common divisor of given numbers."
-BRAIN_PRIME_RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'."
-BRAIN_EVEN_RULES = "Answer 'yes' if the number is even, otherwise answer 'no'."
+BRAIN_PRIME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+BRAIN_EVEN_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 PROGRESSION_LENGTH = 10
 
